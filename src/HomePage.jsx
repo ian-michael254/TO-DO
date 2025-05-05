29b9d8e411ed3dfb65e071df4089d991ade0e601
ian-michael254 <ian.michael.codes@gmail.com>
2025-05-05 22:21:55 +0300

    Project Set up and configs

---
+++ src/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+        <section className='w-dvw h-lvh py-10 px-5 my-1 mx-auto bg-slate-400'>
+            <h1 className="text-4xl font-body">Hello World</h1>
+        </section>
+        <section className='w-dvw h-lvh py-10 px-5 my-1 mx-auto bg-purple-primary'></section>
+        <section className='w-dvw h-lvh py-10 px-5 my-1 mx-auto bg-slate-400'></section>
+        <section className='w-dvw h-lvh py-10 px-5 my-1 mx-auto bg-brown-primary'></section>
+        <section className='w-dvw h-lvh py-10 px-5 my-1 mx-auto bg-slate-400'></section>
+    </>
+  )
+}
+
+export default HomePage
